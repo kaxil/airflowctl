454082053434cd131ad46e2c0a2993c6e62a12cb
Kaxil Naik <kaxilnaik@gmail.com>
2023-08-13 05:34:59 +0100

    Working prototype

--- airflowctl/__main__.py
+++ airflowctl/__main__.py
@@ -1,24 +1,29 @@
+import hashlib
 import os
 import subprocess
 import venv
 
+import httpx
 import typer
 import yaml
 from dotenv import load_dotenv
+from rich.console import Console
+from rich.table import Table
 
 app = typer.Typer()
 
-
 CONFIG_FILENAME = "config.yaml"
+PROJECTS_DB_FILE = os.path.expanduser("~/.airflowctl_projects.yaml")
 
 
-def create_project(config_path: str):
+def create_project(project_name: str):
+    config_path = os.path.abspath(project_name)
     # Create the project directory structure
     os.makedirs(config_path, exist_ok=True)
     dags_dir = os.path.join(config_path, "dags")
     os.makedirs(dags_dir, exist_ok=True)
 
-    airflow_version = "2.6.0"
+    airflow_version = get_latest_airflow_version()
     python_version = "3.10"
     # Initialize the config file
     config_file = os.path.join(config_path, CONFIG_FILENAME)
@@ -37,12 +42,25 @@ def create_project(config_path: str):
     typer.echo(f"Airflow project initialized in {config_path}")
 
 
+def get_latest_airflow_version(verbose: bool = False) -> str:
+    try:
+        with httpx.Client() as client:
+            response = client.get("https://pypi.org/pypi/apache-airflow/json")
+            data = response.json()
+            latest_version = data["info"]["version"]
+            if verbose:
+                print(f"Latest Apache Airflow version detected: [bold cyan]{latest_version}[/bold cyan]")
+            return latest_version
+    except (httpx.RequestError, KeyError) as e:
+        if verbose:
+            print(f"[bold red]Error occurred while retrieving latest version: {e}[/bold red]")
+            print("[bold yellow]Defaulting to Apache Airflow version 2.7.0[/bold yellow]")
+        return "2.7.0"
+
+
 @app.command()
 def init(
-    project_name: str = typer.Argument(
-        default=".",
-        help="Name of the Airflow project to be initialized.",
-    )
+    project_name: str = typer.Argument(default=".", help="Name of the Airflow project to be initialized.")
 ):
     """
     Initialize a new Airflow project.
@@ -67,6 +85,34 @@ def create_env_file(project_path: str):
     env_file_path = os.path.join(project_path, ".env")
     with open(env_file_path, "w") as env_file:
         env_file.write(f"AIRFLOW_HOME={project_path}\n")
+
+
+def get_project_hash(config: dict):
+    config_hash = hashlib.sha256(str(config).encode("utf-8")).hexdigest()
+    return config_hash
+
+
+def store_project_info(project_name: str, project_path: str):
+    projects = load_projects()
+    config_file = os.path.join(project_path, CONFIG_FILENAME)
+    with open(config_file) as f:
+        config = yaml.safe_load(f)
+    config_hash = get_project_hash(config)
+    projects = [
+        {
+            "name": project_name,
+            "path": project_path,
+            "config_hash": config_hash,
+            "config_file": CONFIG_FILENAME,
+            "airflow_version": config.get("airflow_version"),
+            "python_version": config.get("python_version"),
+            "venv_path": config.get(
+                "venv_path", f".venv/airflow_{config.get('airflow_version')}_py{config.get('python_version')}"
+            ),
+        }
+    ]
+    with open(PROJECTS_DB_FILE, "w") as f:
+        yaml.dump(projects, f)
 
 
 @app.command()
@@ -99,10 +145,9 @@ def build(
     # Create .env file with AIRFLOW_HOME set to project directory
     create_env_file(project_path)
 
+    store_project_info(os.path.basename(project_path), project_path)
+
     typer.echo("Airflow project built successfully.")
-
-
-PROJECTS_DB_FILE = os.path.expanduser("~/.airflowctl_projects.yaml")
 
 
 def load_projects():
@@ -121,9 +166,21 @@ def list_cmd():
         typer.echo("No projects found.")
         return
 
-    typer.echo("List of projects:")
-    for name, path in projects.items():
-        typer.echo(f"{name}: {path}")
+    console = Console()
+    table = Table(show_header=True, header_style="bold magenta")
+    table.add_column("Project Name")
+    table.add_column("Airflow Version")
+    table.add_column("Python Version")
+    table.add_column("Project Path")
+
+    for project in projects:
+        name = project.get("name")
+        airflow_version = project.get("airflow_version")
+        python_version = project.get("python_version")
+        path = project.get("path")
+        table.add_row(name, airflow_version, python_version, path)
+
+    console.print(table)
 
 
 def source_env_file(env_file: str):
@@ -172,7 +229,16 @@ def start(
     os.environ["AIRFLOW__DATABASE__SQL_ALCHEMY_CONN"] = "sqlite:///" + os.path.abspath(
         os.path.join(project_path, "airflow.db")
     )
-    venv_path = os.path.abspath(config.get("venv_path", os.path.join(project_path, ".venv/airflow")))
+
+    venv_path = os.path.abspath(
+        config.get(
+            "venv_path",
+            os.path.join(
+                project_path,
+                f".venv/airflow_{config.get('airflow_version')}_py{config.get('python_version')}",
+            ),
+        )
+    )
     activate_cmd = activate_virtualenv(venv_path)
 
     try:
